57965df9ce05479add5168f83822cab8b18eb9a5
Xurlz <charlessena2010@bol.com.br>
2020-10-18 19:56:03 -0300

    Implementação  de  redirecionamento

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -24,6 +24,6 @@ class SeriesController extends Controller
 
         $serie = Serie::create($request->all());
 
-        echo "Série com id {$serie->id} criada: {$serie->nome}.";
+        return redirect('/series');
     }
 }
