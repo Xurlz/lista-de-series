088441ccc31d28d39138266af883d534749e12c9
Xurlz <charlessena2010@bol.com.br>
2020-10-18 18:45:14 -0300

    inclusão de rota para armazenamento em banco de dados

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,5 @@ Route::get('login', function()
 });
 
 Route::get('/series', 'App\Http\Controllers\SeriesController@index');
-
 Route::get('/series/criar', 'App\Http\Controllers\SeriesController@create');
+Route::post('/series/criar', 'App\Http\Controllers\SeriesController@store');
