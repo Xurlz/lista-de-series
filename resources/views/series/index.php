c8ae6c7ee8ea7766414ab44b5b140d120d5d2160
Xurlz <charlessena2010@bol.com.br>
2020-10-18 13:06:31 -0300

    Alterações de estilo em view series

--- resources/views/series/index.php
+++ resources/views/series/index.php
@@ -7,11 +7,16 @@
     <title>Controle de series 📼</title>
 </head>
 <body>
-    <h1>📽🎞</h1>
-    <ul>
-    <?php foreach($series as $serie): ?>
-    <li><?= $serie; ?></li>
-    <?php endforeach; ?>
-    </ul>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>🎞Séries</h1>
+        </div>
+        <a href="#" class="btn btn-dark mb-2">Adicionar</a>
+        <ul class="list-group">
+        <?php foreach($series as $serie): ?>
+        <li class="list-group-item"><?= $serie; ?></li>
+        <?php endforeach; ?>
+        </ul>
+    </div>
 </body>
 </html>
