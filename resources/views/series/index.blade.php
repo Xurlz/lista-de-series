879569e0e9b5892ebc58f28cfecf7c6f1e6d7f7a
Xurlz <charlessena2010@bol.com.br>
2020-10-19 19:27:08 -0300

    Inclusão  de  flex  com  font-awesome

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -15,13 +15,15 @@
 
 <ul class="list-group">
     @foreach($series as $serie)
-    <li class="list-group-item">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
         {{$serie->nome}}
         <form method='post' action="series/{{$serie->id }}"
         onsubmit = 'return confirm("tem certeza que quer remover {{ $serie->nome }}?")'>
             @csrf 
             @method('DELETE')
-            <button class="btn btn-danger">Excluir</button>
+            <button class="btn-sm btn-danger">
+                <i class='far fa-trash-alt'></i>
+            </button>
         </form>
     </li>
     @endforeach
