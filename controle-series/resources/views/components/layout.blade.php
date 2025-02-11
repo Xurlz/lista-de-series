82dc35d099be665ab7fd6489065d09b15c48e1a3
Charles Sena <charlessena@gokeitecnologia.com.br>
2025-02-11 18:16:44 -0300

    WIP - "Aula 3" - Implementation

---
+++ controle-series/resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> {{ $title }} - Series Management </title>
+</head>
+<body>
+<h1>{{ $title }}</h1>
+{{ $slot }}
+</body>
+</html>
