7beddd8a4c531aa9fecc3d437374167451f2778e
Charles Sena <charlessena@gokeitecnologia.com.br>
2025-02-11 18:16:44 -0300

    "Aula 3" - Implementation

---
+++ controle-series/resources/views/series/create.blade.php
@@ -0,0 +1,6 @@
+<x-layout title="Nova Série">
+    <form action="" method="post">
+        <label for="name">Nome:</label>
+        <input type="text" id="nome" name="nome"/>
+    </form>
+</x-layout>
