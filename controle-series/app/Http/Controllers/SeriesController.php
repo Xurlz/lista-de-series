fc77f209f02838264da82d463720cd678e31c467
Charles Sena <charlessena@gokeitecnologia.com.br>
2025-04-22 18:16:00 +0000

    "Aula 5" - Implementation
    - Migration
    - Model
    - ORM

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -2,25 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 
-class SeriesController extends Controller {
-
-    function index()
+class SeriesController extends Controller
+{
+    public function index()
     {
-        $series = [
-            'Lupin',
-            'Senna',
-            'Kobra Kai'
-        ];
+        $series = Serie::query()->orderBy('name')->get();
 
-        return view('series.index',compact('series'));
+        return view('series.index', compact('series'));
 
     }
 
-    function create()
+    public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $serieName = $request->input('name');
+        $serie = new Serie();
+        $serie->name = $serieName;
+        $serie->save();
+        return redirect(to: '/series');
     }
 
 }
--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -1,8 +1,8 @@
 <x-layout title="Séries">
     <ul class="list-group">
         <a href="/series/create" class="list-group-item list-group-item-action list-group-item-dark ">Adicionar</a>
-        @foreach($series as $serie)
-            <li class="list-group-item">{{ $serie }}</li>
+        @foreach ($series as $serie)
+            <li class="list-group-item">{{ $serie->name }}</li>
         @endforeach
     </ul>
 </x-layout>
