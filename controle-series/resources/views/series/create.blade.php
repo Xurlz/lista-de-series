b99a878518b16ab500edcc7a12cd6385a1dea401
Charles Sena <charlessena@gokeitecnologia.com.br>
2025-04-22 18:16:00 +0000

    "Aula 5" - Implementation
    - Migration
    - Model
    - ORM

--- controle-series/resources/views/series/create.blade.php
+++ controle-series/resources/views/series/create.blade.php
@@ -1,8 +1,9 @@
 <x-layout title="Nova Série">
-    <form action="" method="post">
+    <form action="/series/save" method="post">
+        @csrf
         <label for="name" class="form-label">Nome:</label>
         <div class="input-group">
-            <input type="text" id="name" name="nome" class="input-group-text"/>
+            <input type="text" id="name" name="name" class="input-group-text" />
             <button type="submit" class="btn btn-outline btn-outline-primary">Adicionar</button>
         </div>
     </form>
